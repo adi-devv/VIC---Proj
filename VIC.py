bad334652b3683dd3cc4a3130bdaa3d3463aca09
adi-devv <aaditsingal7859@gmail.com>
2024-07-07 19:57:50 +0530

    init

--- VIC.py
+++ VIC.py
@@ -2,12 +2,21 @@ from pynput import keyboard
 import time
 import subprocess
 
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+
+
+def search(w):
+    driver = webdriver.Chrome(options=options)
+    driver.get(f"https://www.google.com/search?q={w}")
 
 def lock_windows():
     try:
-        subprocess.run("rundll32.exe user32.dll,LockWorkStation", shell=True)
+        subprocess.run('rundll32.exe user32.dll,LockWorkStation', shell=True)
     except Exception as e:
-        print(f"Error: {e}")
+        print(f'Error: {e}')
 
 
 class SystemLockdown:
@@ -26,6 +35,6 @@ class SystemLockdown:
             with keyboard.Listener(on_release=self.initiate()) as key_listener:
                 key_listener.join()
         except Exception as e:
-            print(f"Error in main: {e}")
+            print(f'Error in main: {e}')
 
 # SystemLockdown.on_key_press()
--- VoiceChat.py
+++ VoiceChat.py
@@ -2,7 +2,7 @@ import win32com.client
 import speech_recognition as sr
 import VIC
 
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
+speaker = win32com.client.Dispatch('SAPI.SpVoice')
 speaker.Voice = speaker.GetVoices().Item(1)
 
 
@@ -17,22 +17,29 @@ def takeCommand():
         r.pause_threshold = 1
         audio = r.listen(mic)
         try:
-            print("Recognizing...")
-            q = r.recognize_google(audio, language="en-in")
-            print(f"User said: {q}")
-            say(q)
+            print('Recognizing...')
+            q = r.recognize_google(audio, language='en-in')
+            print(f'User said: {q}')
             return q
         except Exception as e:
-            print(f"Some error occurred please try again..{e}")
+            print(f'Some error occurred please try again..{e}')
 
 
-if __name__ == "__main__":
-    say("Initiating Script")
+if __name__ == '__main__':
+    say('Initiating Script')
+    kw = 'hp '
     while True:
-        print("Listening...")
+        print('Listening...')
         query = takeCommand()
         if query:
             query = query.lower()
-            if "hp" in query:
-                if "lock" in query:
+            if kw in query:
+                if 'lock' in query:
                     VIC.lock_windows()
+
+            if 'search' in query:
+                txt = query.split('search')[1].strip()
+                if txt == '': continue
+                print("Searching:", txt)
+                say("Searching"+txt)
+                VIC.search(txt)
